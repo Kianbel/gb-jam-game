48a81ea64cbf6a1eb834f76057acc88544b3aae2
Kianbel <kirkianbelcina@gmail.com>
2026-09-18 10:30:41 +0800

    update sprites

--- scripts/button.gd
+++ scripts/button.gd
@@ -6,19 +6,34 @@ signal button_triggered
 const NORMAL_COLLISION = 2
 const INVERTED_COLLISION = 3
 
-var is_inverted = false;
+var is_inverted = false
+var is_pushed = false
 
 @onready var area2d = $Area2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
-	setup_collision()
+	setup()
 
-func setup_collision():
+func setup():
 	var parent_group = $".."
 	if parent_group is InvertedGroup:
 		area2d.set_collision_layer_value(NORMAL_COLLISION, false)
 		area2d.set_collision_layer_value(INVERTED_COLLISION, true)
 		is_inverted = true
+		animated_sprite_2d.play("sand_inverted")
+		animated_sprite_2d.stop()
+		animated_sprite_2d.frame = 0
 	else:
 		area2d.set_collision_layer_value(NORMAL_COLLISION, true)
 		area2d.set_collision_layer_value(INVERTED_COLLISION, false)
+		animated_sprite_2d.play("sand_normal")
+		animated_sprite_2d.stop()
+		animated_sprite_2d.frame = 0
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if not is_pushed && body is Player:
+		is_pushed = true
+		if is_inverted: animated_sprite_2d.play("sand_inverted")
+		else: animated_sprite_2d.play("sand_normal")
--- scripts/spike.gd
+++ scripts/spike.gd
@@ -3,6 +3,9 @@ extends Trap
 @export var trigger: TriggerButton
 
 @onready var hitbox = $HitboxComponent
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+var is_triggered = false;
 
 const NORMAL_COLLISION = 2
 const INVERTED_COLLISION = 3
@@ -12,18 +15,29 @@ func _ready() -> void:
 		visible = false
 		trigger.button_triggered.connect(on_trap_triggered)
 	else:
-		setup_collision()
+		setup()
 	
-func setup_collision():
+func setup():
 	var parent_group = $".."
 	if parent_group is InvertedGroup:
 		hitbox.set_collision_layer_value(NORMAL_COLLISION, false)
 		hitbox.set_collision_layer_value(INVERTED_COLLISION, true)
+		animated_sprite_2d.play("sand_inverted")
+		animated_sprite_2d.stop()
+		var frame_count: int = animated_sprite_2d.sprite_frames.get_frame_count(animated_sprite_2d.animation)
+		animated_sprite_2d.frame = randi() % frame_count
+		
 	else:
 		hitbox.set_collision_layer_value(NORMAL_COLLISION, true)
 		hitbox.set_collision_layer_value(INVERTED_COLLISION, false)
+		animated_sprite_2d.play("sand_normal")
+		animated_sprite_2d.stop()
+		var frame_count: int = animated_sprite_2d.sprite_frames.get_frame_count(animated_sprite_2d.animation)
+		animated_sprite_2d.frame = randi() % frame_count
 
 
 func on_trap_triggered():
-	visible = true
-	setup_collision()
+	if not is_triggered: 
+		visible = true
+		setup()
+		is_triggered = true
